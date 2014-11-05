a1189b2e8eb28b02be4560a2c6073f5c1c84e133
John Foushee <johnfoushee@gmail.com>
2014-11-05 15:02:35 -0500

    Base update service to add form mapping.

---
+++ module/FzyForm/src/FzyForm/Service/Update/Base.php
@@ -0,0 +1,39 @@
+<?php
+namespace FzyForm\Service\Update;
+use FzyForm\Annotation\Subform;
+
+class Base extends \FzyCommon\Service\Update\Base {
+	/**
+	 * Array mapping a getter string to a configured form
+	 * @var array
+	 */
+	protected $formMap = array();
+
+	public function getForms()
+	{
+		if (!isset($this->forms)) {
+			$forms = parent::getForms();
+			// set up main form with map
+			/* @var $mainForm \Zend\Form\Form */
+			$mainForm = $forms[static::MAIN_TAG];
+			$options = $mainForm->getOptions();
+			$options[Subform::OPTION_MAP_ARRAY] = $this->formMap;
+			$mainForm->setOptions($options);
+		}
+		return $this->forms;
+	}
+
+	public function setFormMapEntry($getterString, $configuredForm)
+	{
+		$this->formMap[$getterString] = $configuredForm;
+
+		return $this;
+	}
+
+	public function reset()
+	{
+		$this->formMap = array();
+		return parent::reset();
+	}
+
+}
