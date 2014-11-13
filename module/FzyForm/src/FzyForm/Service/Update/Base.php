ca79eb69f761efab0e3a33d6413bec75c8ada9f5
John Foushee <johnfoushee@gmail.com>
2014-11-13 14:30:37 -0500

    Updating zf2 classmap.

--- module/FzyForm/src/FzyForm/Service/Update/Base.php
+++ module/FzyForm/src/FzyForm/Service/Update/Base.php
@@ -2,38 +2,41 @@
 namespace FzyForm\Service\Update;
 use FzyForm\Annotation\Subform;
 
-class Base extends \FzyCommon\Service\Update\Base {
-	/**
-	 * Array mapping a getter string to a configured form
-	 * @var array
-	 */
-	protected $formMap = array();
+class Base extends \FzyCommon\Service\Update\Base
+{
+    /**
+     * Array mapping a getter string to a configured form
+     * @var array
+     */
+    protected $formMap = array();
 
-	public function getForms()
-	{
-		if (!isset($this->forms)) {
-			$forms = parent::getForms();
-			// set up main form with map
-			/* @var $mainForm \Zend\Form\Form */
-			$mainForm = $forms[static::MAIN_TAG];
-			$options = $mainForm->getOptions();
-			$options[Subform::OPTION_MAP_ARRAY] = $this->formMap;
-			$mainForm->setOptions($options);
-		}
-		return $this->forms;
-	}
+    public function getForms()
+    {
+        if (!isset($this->forms)) {
+            $forms = parent::getForms();
+            // set up main form with map
+            /* @var $mainForm \Zend\Form\Form */
+            $mainForm = $forms[static::MAIN_TAG];
+            $options = $mainForm->getOptions();
+            $options[Subform::OPTION_MAP_ARRAY] = $this->formMap;
+            $mainForm->setOptions($options);
+        }
 
-	public function setFormMapEntry($getterString, $configuredForm)
-	{
-		$this->formMap[$getterString] = $configuredForm;
+        return $this->forms;
+    }
 
-		return $this;
-	}
+    public function setFormMapEntry($getterString, $configuredForm)
+    {
+        $this->formMap[$getterString] = $configuredForm;
 
-	public function reset()
-	{
-		$this->formMap = array();
-		return parent::reset();
-	}
+        return $this;
+    }
+
+    public function reset()
+    {
+        $this->formMap = array();
+
+        return parent::reset();
+    }
 
 }
